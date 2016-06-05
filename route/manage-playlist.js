89eee94285bf9de0d1fa8d839d206a9a05938c1e
Stefanie Hansen <stefuhnee@gmail.com>
2016-06-05 16:27:38 -0700

    reorganized file structure

--- route/manage-playlist.js
+++ route/manage-playlist.js
@@ -2,41 +2,9 @@
 
 const router = require('express').Router();
 const request = require('request');
-const passport = require('passport');
-const SpotifyStrategy = require('../node_modules/passport-spotify/lib/passport-spotify/index').Strategy;
 const User = require('../model/user')
 
-
-// passport.use(new SpotifyStrategy({
-//   clientID: process.env.CLIENT_ID = '338fd814034c4c84a756175c3cc3575e',
-//   clientSecret: process.env.CLIENT_SECRET = '7cc52791d5e5460abd51f2513873225a',
-//   callbackURL: 'http://localhost:8888/callback'
-// },
-// function(accessToken, refreshToken, profile, done) {
-//   console.log(accessToken);
-//   User.findOrCreate({ spotifyId: profile.id }, function (err, user) {
-//     return done(err, user);
-//   });
-// }
-// ));
-//
-// router.get('/auth/spotify',
-//   passport.authenticate('spotify'),
-//   function(req, res){
-//     console.log('it got here spotify');
-//     // The request will be redirected to spotify for authentication, so this
-//     // function will not be called.
-//   });
-//
-// router.get('/callback',
-//   passport.authenticate('spotify', { failureRedirect: '/login' }),
-//   function(req, res) {
-//     console.log('it got here callback');
-//     // Successful authentication, redirect home.
-//     res.redirect('/');
-//   });
-
-router.get('/getPlaylist', (req, res) => {
+router.get('/playlist', (req, res) => {
 
   let access_token = req.headers.token;
   let playlist_id = req.headers.playlist;
@@ -48,15 +16,41 @@ router.get('/getPlaylist', (req, res) => {
     headers: {
       Authorization: 'Bearer ' + access_token
     }
-  }, function(error, response, body){
-    if(error) {
+  }, (error, response, body) => {
+    if (error) {
       console.log(error);
     } else {
       console.log(response.statusCode, body);
       res.send(body);
     }
   });
+});
 
+router.post('/create/:id', (req, res) => {
+  let playlistName = req.headers.name;
+  let access_token = req.headers.token;
+  let user_id = req.params.id;
+
+  request({
+    url: `https://api.spotify.com/v1/users/${user_id}/playlists`,
+    method: 'POST',
+    headers: {
+      'Authorization': 'Bearer ' + access_token,
+      'Content-Type': 'application/json'
+    },
+    json: {
+      name: playlistName,
+      public: false
+    }
+  }, (err, response, body) => {
+    console.log(body);
+    if (!body.error && res.statusCode === 200) {
+      return res.json({Message: 'Playlist Created!'});
+    }
+    else {
+      res.json('error', body.error);
+    }
+  });
 });
 
 module.exports = router;
--- route/user-auth.js
+++ route/user-auth.js
@@ -2,132 +2,64 @@
 
 const express = require('express');
 const router = express.Router();
-const request = require('request'); // "Request" library
-const querystring = require('querystring');
+const methodOverride = require('method-override');
 const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const SpotifyStrategy = require('passport-spotify').Strategy;
+const session = require('express-session');
 
-const client_id = process.env.CLIENT_ID; // Your client id
-const client_secret = process.env.CLIENT_SECRET; // Your secret
-const redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
-const stateKey = 'spotify_auth_state';
-
-const generateRandomString = require('../lib/generate-random-string');
+const appKey = process.env.CLIENT_ID; // Your client id
+const appSecret = process.env.CLIENT_SECRET; // Your secret
 
 const User = require('../model/user');
 
-router.use(express.static(__dirname + '/../public'))
-   .use(cookieParser());
+router.use(cookieParser());
+router.use(bodyParser());
+router.use(methodOverride());
+router.use(session({ secret: 'keyboard cat' }));
 
-router.get('/login', (req, res) => {
-
-  let state = generateRandomString(16);
-  res.cookie(stateKey, state);
-
-  // your application requests authorization
-  let scope = 'user-read-private playlist-modify-private';
-  res.redirect('https://accounts.spotify.com/authorize?' +
-    querystring.stringify({
-      response_type: 'code',
-      client_id: client_id,
-      scope: scope,
-      redirect_uri: redirect_uri,
-      state: state
-    }));
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
 });
 
-router.get('/callback', function(req, res) {
-
-  // your application requests refresh and access tokens
-  // after checking the state parameter
-
-  let code = req.query.code || null;
-  let state = req.query.state || null;
-  let storedState = req.cookies ? req.cookies[stateKey] : null;
-
-  if (state === null || state !== storedState) {
-    res.redirect('/#' +
-      querystring.stringify({
-        error: 'state_mismatch'
-      }));
-  } else {
-    res.clearCookie(stateKey);
-    var authOptions = {
-      url: 'https://accounts.spotify.com/api/token',
-      form: {
-        code: code,
-        redirect_uri: redirect_uri,
-        grant_type: 'authorization_code'
-      },
-      headers: {
-        'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
-      },
-      json: true
-    };
-
-    request.post(authOptions, function(error, response, body) {
-      if (!error && response.statusCode === 200) {
-
-        let access_token = body.access_token,
-            refresh_token = body.refresh_token;
-
-        let options = {
-          url: 'https://api.spotify.com/v1/me',
-          headers: { 'Authorization': 'Bearer ' + access_token },
-          json: true
-        };
-
-        // use the access token to access the Spotify Web API
-        request.get(options, function(error, response, body) {
-          console.log(body);
-          let newUser = new User({user_id: body.id});
-
-          User.findOne({user_id: body.id}, (err, user) => {
-            if (err || user) return;
-            newUser.save((err, user) => {
-              if (err) return;
-              console.log('user created');
-            });
-          });
-        });
-
-        // we can also pass the token to the browser to make requests from there
-        res.redirect('/#' +
-          querystring.stringify({
-            access_token: access_token,
-            refresh_token: refresh_token
-          }));
-      } else {
-        res.redirect('/#' +
-          querystring.stringify({
-            error: 'invalid_token'
-          }));
-      }
-    });
-  }
-});
-
-router.get('/refresh_token', function(req, res) {
-
-  // requesting access token from refresh token
-  let refresh_token = req.query.refresh_token;
-  let authOptions = {
-    url: 'https://accounts.spotify.com/api/token',
-    headers: { 'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64')) },
-    form: {
-      grant_type: 'refresh_token',
-      refresh_token: refresh_token
-    },
-    json: true
-  };
-
-  request.post(authOptions, function(error, response, body) {
-    if (!error && response.statusCode === 200) {
-      let access_token = body.access_token;
-      res.send({
-        'access_token': access_token
-      });
-    }
+passport.deserializeUser(function(id, done) {
+  User.findOne( {user_id: id}, function(err, user) {
+    done(err, user);
   });
 });
+
+passport.use(new SpotifyStrategy({
+  clientID: appKey,
+  clientSecret: appSecret,
+  callbackURL: 'http://localhost:8888/callback'
+},
+  (accessToken, refreshToken, profile, done) => {
+    console.log('access', accessToken);
+    User.findOne({ user_id: profile.id }, function (err, user) {
+      return done(err, user);
+    });
+  }
+));
+
+router.get('/', function(req, res){
+  res.render('login.html', { user: req.user });
+});
+
+router.get('/auth/spotify', passport.authenticate('spotify', {scope: ['user-read-email', 'playlist-modify-private'], showDialog: true}), () => {
+  // nothing to see here
+});
+
+router.get('/callback',
+  passport.authenticate('spotify', { failureRedirect: '/' }),
+  function(req, res) {
+    res.redirect('/');
+  });
+
+
+// function ensureAuthenticated(req, res, next) {
+//   if (req.isAuthenticated()) { return next(); }
+//   res.redirect('/login');
+// }
 
 module.exports = router;
