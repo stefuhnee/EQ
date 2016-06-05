d9c298f98c8201e56914b8cb3d82a652f53f0ad2
Stefanie Hansen <stefuhnee@gmail.com>
2016-06-05 10:58:19 -0700

    env variables removed

--- route/user-auth.js
+++ route/user-auth.js
@@ -6,8 +6,8 @@ const request = require('request'); // "Request" library
 const querystring = require('querystring');
 const cookieParser = require('cookie-parser');
 
-const client_id = '338fd814034c4c84a756175c3cc3575e'; // Your client id
-const client_secret = '7cc52791d5e5460abd51f2513873225a'; // Your secret
+const client_id = process.env.CLIENT_ID; // Your client id
+const client_secret = process.env.CLIENT_SECRET; // Your secret
 const redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
 const stateKey = 'spotify_auth_state';
 
