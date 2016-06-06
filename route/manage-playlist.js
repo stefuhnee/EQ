0d38a7532a4db48b28ff6484c45219e15cbe2659
YOUR FULL NAME <YOUR EMAIL ADDRESS>
2016-06-06 16:25:07 -0700

    made delete finally work and refactoring for superagent

--- route/manage-playlist.js
+++ route/manage-playlist.js
@@ -1,28 +1,45 @@
 'use strict';
 
 const router = require('express').Router();
-const request = require('request');
+const dumrequest = require('request');
 const User = require('../model/user');
+const request = require('superagent');
 let user_id;
 let playlist_id;
 let access_token;
 
+// router.get('/playlist', (req, res) => {
+//   // ERROR HANDLING IF NO PLAYLIST
+//   dumrequest({
+//     url: `https://api.spotify.com/v1/users/${user_id}/playlists/${playlist_id}`,
+//     method: 'GET',
+//     headers: {
+//       Authorization: 'Bearer ' + access_token
+//     }
+//   }, (error, response, body) => {
+//     if (error) {
+//       console.log(error);
+//     } else {
+//       console.log(response.statusCode, body);
+//       res.json({message: 'Here is a playlist', data:body});
+//     }
+//   });
+// });
+
 router.get('/playlist', (req, res) => {
-  // ERROR HANDLING IF NO PLAYLIST
-  request({
-    url: `https://api.spotify.com/v1/users/${user_id}/playlists/${playlist_id}`,
-    method: 'GET',
-    headers: {
-      Authorization: 'Bearer ' + access_token
-    }
-  }, (error, response, body) => {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log(response.statusCode, body);
-      res.json({message: 'Here is a playlist', data:body});
-    }
-  });
+  playlist_id = req.headers.name;
+  user_id = req.headers.id;
+  access_token = req.headers.token;
+  let pTracks;
+  request
+  .get(`https://api.spotify.com/v1/users/${user_id}/playlists/${playlist_id}`)
+  .set('Authorization', 'Bearer ' + access_token)
+  .end((err, res) => {
+    console.log(res.body.tracks.items[0]);
+    if(err) return err;
+    pTracks = res.body.tracks.items;
+  })
+  res.json({tracks: pTracks});
 });
 
 router.post('/create/:id', (req, res) => {
@@ -31,7 +48,7 @@ router.post('/create/:id', (req, res) => {
   access_token = req.headers.token;
   user_id = req.params.id;
 
-  request({
+  dumrequest({
     url: `https://api.spotify.com/v1/users/${user_id}/playlists`,
     method: 'POST',
     headers: {
@@ -58,7 +75,7 @@ router.post('/add/:track', (req, res) => {
   let track = req.params.track;
 
 
-  request({
+  dumrequest({
     url: `https://api.spotify.com/v1/users/${user_id}/playlists/${playlist_id}/tracks`,
     method: 'POST',
     headers: {
@@ -81,26 +98,28 @@ router.post('/add/:track', (req, res) => {
 
 router.delete('/delete/:track', (req, res) => {
   let track = req.params.track;
-  console.log(track);
-  request({
-    url: `https://api.spotify.com/v1/users/${user_id}/playlists/${playlist_id}/tracks`,
-    method: 'DELETE',
-    headers: {
-      'Authorization': 'Bearer ' + access_token,
-      'Content-Type': 'application/json'
-    },
-    json: {
-      uris: [`${track}`]
-    }
-  }, (err, response, body) => {
-    console.log(body);
-    if (!body.error && res.statusCode === 200) {
-      return res.json({Message: 'Track deleted!'});
-    }
-    else {
-      res.json('error', body.error);
-    }
-  });
+  access_token = req.headers.token;
+  user_id = req.headers.id;
+  playlist_id = req.headers.playlist;
+  request
+    .del(`https://api.spotify.com/v1/users/${user_id}/playlists/${playlist_id}/tracks`)
+    .send({
+      'tracks' : [
+        {
+          'uri' : `${track}`
+        }
+      ]
+    })
+    .set(
+      'Authorization', 'Bearer ' + access_token
+    )
+    .set(
+      'Accept', 'application/json'
+    )
+    .end(function(err, res){
+      if(err) return err;
+    });
+    res.send('ending');
 });
 
 module.exports = router;
