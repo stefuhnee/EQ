b4e0b80297f0becd51c91a94f243daf0be088bb8
Stefanie Hansen <stefuhnee@gmail.com>
2016-06-06 15:25:12 -0700

    working on auto-refreshing token

--- model/user.js
+++ model/user.js
@@ -3,7 +3,10 @@
 const mongoose = require('mongoose');
 
 const User = new mongoose.Schema({
-  user_id: {type: String, required: true}
+  user_id: {type: String, required: true},
+  accessToken: {type: String, required: true},
+  refreshToken: {type: String, required: true},
+  tokenExpires: {type: Number, required: true}
 });
 
 module.exports = mongoose.model('user', User);
--- route/manage-playlist.js
+++ route/manage-playlist.js
@@ -3,11 +3,55 @@
 const router = require('express').Router();
 const request = require('request');
 const User = require('../model/user');
+const client_id = process.env.CLIENT_ID; // Your client id
+const client_secret = process.env.CLIENT_SECRET;
 let user_id;
 let playlist_id;
 let access_token;
 
-router.get('/playlist', (req, res) => {
+
+function checkToken(req, res, next) {
+  let time = Date.now();
+
+  User.find({user_id}, (err, user) => {
+    console.log('user', user);
+    let newToken;
+    let newRefresh;
+    if (user.tokenExpires > time) {
+
+      let refresh_token = user.refreshToken;
+      let authOptions = {
+        url: 'https://accounts.spotify.com/api/token',
+        headers: { 'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64')) },
+        form: {
+          grant_type: 'refresh_token',
+          refresh_token: refresh_token
+        },
+        json: true
+      };
+
+      request.post(authOptions, function(error, response, body) {
+        if (!error && response.statusCode === 200) {
+          newToken = body.access_token;
+          if (body.refresh_token) {
+            newRefresh = body.refresh_token;
+            User.findOneAndUpdate({user_id}, {refreshToken: newRefresh, accessToken: newToken});
+            return next();
+          }
+          else {
+            User.findOneAndUpdate({user_id}, {refreshToken: null, accessToken: newToken});
+            return next();
+          }
+        }
+        else {
+          res.send('error refreshing token');
+        }
+      });
+    }
+  });
+}
+
+router.get('/playlist', checkToken, (req, res) => {
   // ERROR HANDLING IF NO PLAYLIST
   request({
     url: `https://api.spotify.com/v1/users/${user_id}/playlists/${playlist_id}`,
@@ -53,7 +97,7 @@ router.post('/create/:id', (req, res) => {
   });
 });
 
-router.post('/add/:track', (req, res) => {
+router.post('/add/:track', checkToken, (req, res) => {
 
   let track = req.params.track;
 
