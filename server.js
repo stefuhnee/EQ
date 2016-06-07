c4d5d4fc91bf25d5bb4d81381a602f09d6b05e99
jdar22169 <mmow313@gmail.com>
2016-06-07 16:28:00 -0700

    changed mongo

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const spotifyAuthRouter = require('./route/spotify-auth');
 const playlistRouter = require('./route/manage-playlist');
 const userAuth = require('./route/user-routes');
 
-const dbPort = process.env.MONGOLAB_URI || 'mongodb://localhost/dev_db';
+const dbPort = process.env.MONGODB_URI || 'mongodb://localhost/dev_db';
 mongoose.connect(dbPort);
 
 
